dcf3db793d48486d7659be19f4a245708244a10c
mukul71 <mukul71@gmail.com>
2024-04-20 17:36:18 +0600

    JavaScript added

---
+++ CSS_variable/overrideVariable.js
@@ -0,0 +1,16 @@
+//Get the root element
+var r = document.querySelector(':root');
+
+//Create a function for getting a variable value
+function myFunction_get(){
+    //Get the styles (properties and values) for the root
+    var rs = getComputedStyle(r);
+    //Alert the variable of the --blue.variable
+    alert('The value of --blue is: '+rs.getPropertyValue(--blue));
+}
+
+//create a function for setting a variable value
+function myFunction_set(){
+    //set the value of --blue to lightblue
+    r.style.setProperty('--blue', 'lightblue');
+}
